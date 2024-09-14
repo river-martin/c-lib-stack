d88077c59933173706d2ab4b4919833b1c3db577
river-martin <rivermartin128@gmail.com>
2024-09-14 14:20:15 +0200

    Update references to functions from `stack.h`

--- tests/test_stack.c
+++ tests/test_stack.c
@@ -7,7 +7,7 @@
 
 vp_stack _stack = { .top = NULL };
 
-#define pop_intp(stack) ((int *) pop(stack))
+#define pop_intp(stack) ((int *) vp_stack_pop(stack))
 
 /**
  * @brief Test the stack with int pointers.
@@ -17,7 +17,7 @@ void test_int_stack(vp_stack *stack)
     for (int i = 0; i < 10; i++) {
         int *n = malloc(sizeof(int));
         *n     = i;
-        push(stack, n);
+        vp_stack_push(stack, n);
     }
     for (int i = 9; i >= 0; i--) {
         int *p = pop_intp(stack);
@@ -26,7 +26,7 @@ void test_int_stack(vp_stack *stack)
         free(p);
     }
     assert(stack->top == NULL);
-    assert(pop(stack) == NULL);
+    assert(vp_stack_pop(stack) == NULL);
     assert(stack->top == NULL);
     fprintf(stdout, "%s: passed\n", __func__);
 }
@@ -38,14 +38,14 @@ void test_str_stack(vp_stack *stack)
 {
     char *strs[] = { "Hello", "World", "C", "Programming", "Language" };
     for (size_t i = 0; i < sizeof(strs) / sizeof(*strs); i++)
-        push(stack, strs[i]);
+        vp_stack_push(stack, strs[i]);
     for (size_t i = sizeof(strs) / sizeof(*strs); i > 0; i--) {
-        char *str = pop(stack);
+        char *str = vp_stack_pop(stack);
         assert(str != NULL);
         assert(strcmp(str, strs[i - 1]) == 0);
     }
     assert(stack->top == NULL);
-    assert(pop(stack) == NULL);
+    assert(vp_stack_pop(stack) == NULL);
     assert(stack->top == NULL);
     fprintf(stdout, "%s: passed\n", __func__);
 }
@@ -57,7 +57,7 @@ int main(void)
     assert(stack.top == NULL);
     test_str_stack(&stack);
     assert(stack.top == NULL);
-    pop(&stack);
+    vp_stack_pop(&stack);
     assert(stack.top == NULL);
     printf("All tests passed\n");
     return EXIT_SUCCESS;
